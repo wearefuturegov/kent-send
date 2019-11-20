d2c6a3b217f71fc51223fba398c5234339ebe0de
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-11-20 21:06:44 +0000

    add theme

---
+++ components/_theme.js
@@ -0,0 +1,10 @@
+export default {
+    teal: "#248096",
+    darkTeal: "#103A44",
+    purple: "#8E1065",
+    darkPurple: "#600B44",
+    red: "#C42927",
+    lightGrey: "#F5F5F5",
+    lightBlue: "#EEF6F8",
+    active: "#ffc107",
+}
