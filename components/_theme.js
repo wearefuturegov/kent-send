9db033d1a2c000eaab9f39e9e26e240633f58f96
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-11-21 09:05:16 +0000

    header and search wip

--- components/_theme.js
+++ components/_theme.js
@@ -4,7 +4,9 @@ export default {
     purple: "#8E1065",
     darkPurple: "#600B44",
     red: "#C42927",
+    placeholder: "#56595f",
+    grey: "rgba(9,38,45,.4)",
     lightGrey: "#F5F5F5",
     lightBlue: "#EEF6F8",
-    active: "#ffc107",
+    focus: "#ffc107",
 }
