89733f1965832c42faf43872bc63d03c3c78948a
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-11-22 11:02:21 +0000

    basic page styles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
 import Layout from "../components/Layout"
+import styled from "styled-components"
+import theme from "../components/_theme"
+
+const Main = styled.main`
+    max-width: 970px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 30px 15px;
+    color: ${theme.darkTeal};
+`
+
+const PageTitle = styled.h1`
+    font-size: 2.5rem;
+    @media screen and (min-width: 56.25rem){
+        font-size: 3rem;
+    }
+`
 
 export default () =>
     <Layout>
-        <h1>Test page</h1>
+        <Main>
+            <PageTitle>Test page</PageTitle>
+        </Main>
     </Layout>
