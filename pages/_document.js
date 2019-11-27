21d009514315c40873d4d04ab4bcd83d114eaf08
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-11-27 14:07:13 +0000

    add title and favicon

--- pages/_document.js
+++ pages/_document.js
@@ -30,7 +30,10 @@ export default class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
+                    <title>Explore your options - Kent County Councl</title>
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                    <link rel="shortcut icon" type="image/x-icon" href="//assets.kcc.digital/kentgov/dist/images/favicon.ico"/>
+                    <link rel="apple-touch-icon" href="//assets.kcc.digital/kentgov/dist/images/apple-touch-icon.png"/>
                     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700&display=swap" rel="stylesheet"/> 
                     {this.props.styles}
                 </Head>
